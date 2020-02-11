ae0e62ca43d2dac0a291c4cc7c0634e80c20628f
Elias <eliasdamianlopez@gmail.com>
2020-02-11 07:33:32 -0500

    message

---
+++ app.js
@@ -0,0 +1,15 @@
+const mat = [
+  ["Pablo", "Maria", "Pedro"],
+  ["Diana", "Juan", "Federico"],
+  ["Roberto", "Daniel", "Sandra"],
+  ["1","2", "3"]
+];
+
+// escribe tu código debajo de esta línea
+
+for (let i=0; i < mat.length; i++){
+  for (let j=0; j < mat[i]; j++){
+    console.log (mat[i] [j]);
+  }
+
+}
